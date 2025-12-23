c0342c6f5b36385bb1727d3cc9c19bb649bec847
Chellapandian-J <jchellapandian20@gmail.com>
2025-12-23 22:49:18 +0530

    added hashmap solution also

--- leetcode/String/205. Isomorphic Strings.java
+++ leetcode/String/205. Isomorphic Strings.java
@@ -14,3 +14,24 @@ class Solution {
         return true;
     }
 }
+//using hashmap but above(array)method is safe
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> maps = new HashMap<>();
+        HashMap<Character, Integer> mapt = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            Integer st = maps.put(a, i + 1);
+            Integer ts = mapt.put(b, i + 1);
+
+            if (!Objects.equals(st, ts)) return false;
+        }
+        return true;
+    }
+}
