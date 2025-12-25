3a386b95fbd34c726eaf1b721a5a10aec3df1fe4
Chellapandian-J <jchellapandian20@gmail.com>
2025-12-25 10:12:53 +0530

    solution added

---
+++ geeksforgeeks/string/count-substring.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int countSubstring(String s) {
+        int length=s.length();
+        int left=0;
+        int count=0;
+        int[] freq=new int[3];
+        for(int right=0;right<length;right++){
+            freq[s.charAt(right)-'a']++;
+            while(freq[0]>0 && freq[1]>0 && freq[2]>0){
+                count+=length-right;
+                freq[s.charAt(left)-'a']--;
+                left++;
+            }
+        }
+        return count;
+    }
+}
