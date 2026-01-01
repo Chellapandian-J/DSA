9aa0a85c8eb68254e29067a7ea4480d9ed18c054
Chellapandian-J <jchellapandian20@gmail.com>
2026-01-01 18:14:55 +0530

    solution added

---
+++ leetcode/linked list/148. Sort List.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=null;
+        ListNode left=sortList(head);
+        ListNode right=sortList(slow);
+        return merge(left,right);
+    }
+    private static ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode();
+        ListNode curr=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                curr.next=l1;
+                l1=l1.next;
+            }
+            else{
+                curr.next=l2;
+                l2=l2.next;
+            }
+            curr=curr.next;
+        }
+        curr.next=(l1!=null)?l1:l2;
+        return dummy.next;
+    }
+}
+/*
+Example 1:
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+Time Complexity	            :   O(n log n)
+Auxiliary Space (recursive)	:  O(log n)
+Auxiliary Space (iterative)	:  O(1)
