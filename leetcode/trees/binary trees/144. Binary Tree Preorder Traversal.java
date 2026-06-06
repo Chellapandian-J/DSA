11010cedf6a324b4b91d6422086b831c3e670254
Chellapandian-J <jchellapandian20@gmail.com>
2026-06-06 20:45:34 +0530

    solution added

--- leetcode/trees/binary trees/144. Binary Tree Preorder Traversal.java
+++ leetcode/trees/binary trees/144. Binary Tree Preorder Traversal.java
@@ -34,6 +34,14 @@ Example 1:
 Input: root = [1,null,2,3]
 Output: [1,2,3]
 
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [1,2,4,5,6,7,3,8,9]
+
+Example 3:
+Input: root = []
+Output: []
+
 time complexity: O(N)
 space complexity: O(H) H-height of tree
 */
